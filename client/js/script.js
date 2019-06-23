fa82594b6385f7591f8c4219b469eca938f19d06
NikolaCirisanovic <cirisanovic.nikola@outlook.de>
2019-06-23 17:26:54 +0700

    adding some comments

--- client/js/script.js
+++ client/js/script.js
@@ -3,10 +3,12 @@ window.onload = () => {
         // Target the place in the DOM where I'm going to insert the data from API
         let ul = document.getElementById('users')
         let users = res.data
+        // Logging to the console to see the data structure
         console.log(users)
+        // Looping through each person and adding a bootstrap card for each person. Given name, email, city and zipcode
         users.forEach(u => {
             ul.insertAdjacentHTML("afterbegin", `<div class="card" style="width: 18rem; margin:20px; display: inline-block">
-            <div class="card-header">
+            <div class="card-header"> 
               Name: ${u.name}
             </div>
             <ul class="list-group list-group-flush">
@@ -17,6 +19,5 @@ window.onload = () => {
           </div>`)
         });
     })
-
 
 }
