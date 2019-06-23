30ce495b3e1a4747a290ec9f577149c1ad1aaf51
NikolaCirisanovic <cirisanovic.nikola@outlook.de>
2019-06-23 16:08:53 +0700

    setting up the packages, inserting bootstrap boilerplate

---
+++ index.js
@@ -0,0 +1,21 @@
+// Config, importing the modules
+
+const express = require('express')
+const path = require('path')
+const app = express()
+
+// Serving every file inside of the client folder
+app.use(express.static(path.join(__dirname, 'client')))
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '/client/index.html'))
+	console.log('__dirname', __dirname)
+})
+
+
+// Run Server, execute the method to listen to requests on the specified port
+
+app.listen(3300, () => {
+	console.log('Server listening on port 3300');
+})
+
