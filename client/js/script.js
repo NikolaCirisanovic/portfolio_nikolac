baba4c37ba10a41e56fbc01090ed22f4a6630105
NikolaCirisanovic <cirisanovic.nikola@outlook.de>
2019-06-24 11:39:14 +0700

    rm localhost

--- client/js/script.js
+++ client/js/script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    axios.get('http://localhost:3300/api/products').then((res) => {
+    axios.get('/api/products').then((res) => {
         // Target the place in the DOM where I'm going to insert the data from API
         let ul = document.getElementById('users')
         let users = res.data
